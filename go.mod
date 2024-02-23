058c37c2b5d1bf58bf3f42920c0d4e92af281daa
Syed Khalid <magmastar167@gmail.com>
2024-02-23 13:46:15 +0530

    login - signup ✅ breakup needed

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module JimD
 
 go 1.21.6
 
-require github.com/a-h/templ v0.2.543
-
 require github.com/lib/pq v1.10.9
+
+require golang.org/x/crypto v0.19.0
--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
-	// "github.com/a-h/templ"
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 	"time"
+	"unicode"
+
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func serveHTML(mux *http.ServeMux, path, templateFile string) {
 	mux.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		tmp := template.Must(template.ParseFiles(templateFile))
 		tmp.Execute(w, nil)
-		// fmt.Println(r)
 	})
 }
 
@@ -24,7 +26,7 @@ const (
 	port     = 5432
 	user     = "postgres"
 	password = "167865"
-	dbname   = "users"
+	dbname   = "postgres"
 )
 
 func main() {
@@ -63,15 +65,13 @@ func main() {
 		switch r.Method {
 		case "POST":
 			r.ParseForm()
-			u := User{
-				username: r.Form.Get("username"),
-				email:    "",
-				password: r.Form.Get("password"),
-			}
+			username := r.Form.Get("username")
+			password := r.Form.Get("password")
+
 			var flag string
-			if !userExists(db, u) {
-				flag = "Incorrect Username or You don't exist!"
-			} else if !correctPwd(db, u) {
+			if !userExists(db, username) {
+				flag = "You don't exist!"
+			} else if !correctPwd(db, username, password) {
 				flag = "Incorrect Password"
 			} else {
 				flag = "Successfully logged in!"
@@ -88,7 +88,28 @@ func main() {
 		switch r.Method {
 		case "POST":
 			r.ParseForm()
-			insertUser(db, User{r.Form.Get("username"), r.Form.Get("email"), r.Form.Get("password")})
+
+			username := r.Form.Get("username")
+			email := r.Form.Get("email")
+			password := r.Form.Get("password")
+			confirm_password := r.Form.Get("confirm_password")
+			cps := checkPasswordStrength(password)
+			var flag string
+			if userExists(db, username) {
+				flag = "User already exists!"
+			} else if emailExists(db, email) {
+				flag = "Email already in use!"
+			} else if password != confirm_password {
+				flag = "Passwords don't match"
+			} else if cps != "" {
+				flag = cps
+			} else {
+				flag = "Signup successful!"
+				hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+				insertUser(db, User{username, email, string(hashedPassword)})
+			}
+			tmp := template.Must(template.ParseFiles("./templates/dot.html"))
+			tmp.Execute(w, flag)
 		default:
 			fmt.Println("Default")
 		}
@@ -104,12 +125,52 @@ type User struct {
 	password string
 }
 
+func checkPasswordStrength(password string) string {
+
+	if len(password) < 8 {
+		return "Password must be at least 8 characters long."
+	}
+
+	containsUppercase := false
+	for _, char := range password {
+		if unicode.IsUpper(char) {
+			containsUppercase = true
+			break
+		}
+	}
+	if !containsUppercase {
+		return "Password must contain at least one uppercase letter."
+	}
+
+	containsLowercase := false
+	for _, char := range password {
+		if unicode.IsLower(char) {
+			containsLowercase = true
+			break
+		}
+	}
+	if !containsLowercase {
+		return "Password must contain at least one lowercase letter."
+	}
+
+	containsDigit, _ := regexp.MatchString("[0-9]", password)
+	if !containsDigit {
+		return "Password must contain at least one digit."
+	}
+
+	containsSpecialChar, _ := regexp.MatchString(`[!@#$%^&*(),.?":{}|<>]`, password)
+	if !containsSpecialChar {
+		return "Password must contain at least one special character."
+	}
+
+	return "" // Password is strong
+}
 func createUser(db *sql.DB) {
-	q := `CREATE TABLE IF NOT EXISTS uzer(
+	q := `CREATE TABLE IF NOT EXISTS users(
 	id SERIAL PRIMARY KEY, 
-	username VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
-	password VARCHAR(30) NOT NULL
+	username VARCHAR(255) UNIQUE NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR(255) NOT NULL
 	)`
 
 	_, err := db.Exec(q)
@@ -120,22 +181,33 @@ func createUser(db *sql.DB) {
 
 }
 
-func userExists(db *sql.DB, user User) bool {
-	q := "SELECT EXISTS(SELECT 1 FROM uzer WHERE username = $1)"
+func emailExists(db *sql.DB, email string) bool {
+	q := "SELECT EXISTS(SELECT 1 FROM users WHERE email = $1)"
 	var exists bool
-	db.QueryRow(q, user.username).Scan(&exists)
+	db.QueryRow(q, email).Scan(&exists)
 	return exists
 }
 
-func correctPwd(db *sql.DB, user User) bool {
-	q := "SELECT EXISTS(SELECT 1 FROM uzer WHERE username = $1 AND password = $2)"
+func userExists(db *sql.DB, username string) bool {
+	q := "SELECT EXISTS(SELECT 1 FROM users WHERE username = $1)"
 	var exists bool
-	db.QueryRow(q, user.username, user.password).Scan(&exists)
+	db.QueryRow(q, username).Scan(&exists)
+	return exists
+}
+
+func correctPwd(db *sql.DB, username string, password string) (exists bool) {
+	q := "SELECT password FROM users WHERE username = $1"
+	var hashed string
+	db.QueryRow(q, username).Scan(&hashed)
+	b := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(password))
+	if b == nil {
+		exists = true
+	}
 	return exists
 }
 
 func insertUser(db *sql.DB, user User) {
-	q := `INSERT INTO uzer(username, email, password)
+	q := `INSERT INTO users (username, email, password)
 		VALUES ($1, $2, $3) RETURNING id`
 	var pk int
 	err := db.QueryRow(q, user.username, user.email, user.password).Scan(&pk)
